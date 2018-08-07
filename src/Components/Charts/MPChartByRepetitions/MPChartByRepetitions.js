ef48b12faaa0f1da8b130be8ec0092a699c23a77
blik3230 <3blik230@gmail.com>
2018-08-07 16:02:29 +0300

    Easy clearing code for the first chart on the plotly.js

--- src/Components/Charts/MPChartByRepetitions/MPChartByRepetitions.js
+++ src/Components/Charts/MPChartByRepetitions/MPChartByRepetitions.js
@@ -6,58 +6,14 @@ MPChartByRepetition.propTypes = {
     rowData: PropsTypes.array.isRequired
 };
 
+const colors = ['red', 'green', 'blue', 'pink', 'brown', 'orange', 'yellow'];
 
 export default function MPChartByRepetition(props) {
 
-    //const data = convertCharacteristicToChartData(props.rowData);
-
-
-
     const sortedMapProps = getSortedMapPropertiesByNames(props.rowData, ['level', 'part', 'repetition']);
-
-    // console.log(sortedMapProps);
-
-    // создаю массивы массивов в указаной последовательности
     const properties = 'level.part.repetition'.split('.');
-
-
     const structuredSortedDataAsArr = getStructuredSortedData(props.rowData, sortedMapProps, properties);
     const preparedDataWithSeparator = prepareDataWithSeparator(structuredSortedDataAsArr, sortedMapProps);
-
-    console.log('preparedDataWithSeparator', preparedDataWithSeparator);
-
-
-    // console.time('asObj');
-    // const structuredSortedData = getStructuredSortedDataAsObj(props.rowData, sortedMapProps, properties);
-    // console.timeEnd('asObj');
-    //
-    // console.time('asArr');
-    // console.timeEnd('asArr');
-    //
-    // console.time('без рекурсии');
-    // const  structuredSortedDataArr = getStructuredSortedDataDraft(props.rowData, sortedMapProps, 'repetition.level.part');
-    // console.timeEnd('без рекурсии');
-
-    // console.log('-----------------')
-    // console.log('structuredSortedDataAsArr', structuredSortedDataAsArr)
-
-    // const extandedData = expandData(structuredSortedDataAsArr);
-    // console.log('extandedData', extandedData);
-
-    // представление в виде массива работает быстрее чем представление в виде объекта, иногда в 2 раза.
-    // представление в виде объекта расматривалось для удобства доступа к отдельным сущностям измерений по ключевым свойствам.
-    // с помошью объекта карты можно удобно находить нужные измерения и с помошью массива (todo: нужно реализовать соответ. функцию)
-    // !!!
-    // На самом деле разница в производительности не существенная,
-    // оставить два метода и в последствии выбрать тот с которым будет легче обращаться к нужному измерению, с ходу кажется что это объект!!!;
-    // todo: !!! метод который возвращает объект в нутри должен содержать тоже объекты
-    //
-    // console.log('structuredSortedData', structuredSortedData);
-    // console.log('structuredSortedDataAsArr', structuredSortedDataAsArr);
-    // console.log('массив без рекурсии', structuredSortedDataArr);
-
-    const colors = ['red', 'green', 'blue', 'pink', 'brown', 'orange', 'yellow'];
-
     const shapes = [...(new Array(sortedMapProps.level.length - 1))]
         .map((level, index) => {
             const pos = (index + 1) * sortedMapProps.part.length + index;
@@ -87,32 +43,7 @@ export default function MPChartByRepetition(props) {
             tickvals: preparedDataWithSeparator.tickValue,
             ticktext: preparedDataWithSeparator.tickText
         },
-        shapes: shapes/*[
-            {
-                type: "line",
-                xref: "x",
-                yref: "paper",
-                x0: 3,
-                x1: 3,
-                y0: 0,
-                y1: 1,
-                line: {
-                    color: "#ccc"
-                }
-            },
-            {
-                type: "line",
-                xref: "x",
-                yref: "paper",
-                x0: 7,
-                x1: 7,
-                y0: 0,
-                y1: 1,
-                line: {
-                    color: "#ccc"
-                }
-            }
-        ]*/
+        shapes: shapes
     };
 
     const data = sortedMapProps.repetition.map((rep, i) => {
@@ -137,6 +68,7 @@ export default function MPChartByRepetition(props) {
     );
 };
 
+// exclusively this chart method
 function prepareDataWithSeparator(structuredData, sortedMapProps) {
     // levels - parts - repetitions
     const ticks =  structuredData.reduce((result, level, index) => {
@@ -191,27 +123,6 @@ function prepareDataWithSeparator(structuredData, sortedMapProps) {
     return r;
 }
 
-function convertCharacteristicToChartData(rowData) {
-    const trace1 = {
-        //x: ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4"],
-        y: [5, 3, 2, null, 1, 5, 3, null, 2, 4, 1],
-        name: 'repetition-1',
-        mode: 'lines+markers',
-        line: {shape: 'linear'},
-        type: 'scatter'
-    };
-    const trace2 = {
-        y: [2, 4, 5, null, 5, 1, 4, null, 4, 2, 5],
-        name: 'repetition-2',
-        mode: 'lines+markers',
-        line: {shape: 'linear', color: 'red'},
-        type: 'scatter'
-    };
-    const trace3 = {};
-
-    return [trace1, trace2, trace3];
-}
-
 // todo: add jsdoc
 function getStructuredSortedData(data, mapProps, props) {
     const rootPropName = props[0];
@@ -227,58 +138,6 @@ function getStructuredSortedData(data, mapProps, props) {
 
         return acc;
     }, []);
-}
-
-
-function getStructuredSortedDataAsObj(data, mapProps, props) {
-    const rootPropName = props[0];
-
-    return mapProps[rootPropName].reduce((acc, mapPropValue) => {
-        const filteredData = data.filter(d => d[rootPropName] === mapPropValue);
-
-        if(props.length > 1) {
-            acc[mapPropValue] = getStructuredSortedData(filteredData, mapProps, props.slice(1));
-            console.log('returned value', acc[mapPropValue]);
-        } else {
-            acc[mapPropValue] = filteredData[0];
-        }
-
-        return acc;
-    }, {});
-}
-
-
-// Возвращает массив массивов рекурсивно в указаной последовательности.
-function getStructuredSortedDataDraft(data, mapProps, format) {
-    const properties = format.split('.');
-
-    const firstPropertyName = properties[0];
-
-    const result = mapProps[firstPropertyName].map(pValue => {
-
-        const items = data.filter(i => {
-            return i[firstPropertyName] === pValue;
-        });
-
-        //const sortedItems = items.sort(getCompareByKeyFn(firstPropertyName));
-        const secondPropertyName = properties[1];
-
-        return mapProps[secondPropertyName].map(spValue => {
-
-            const itemsLv2 = items.filter(iLv2 => iLv2[secondPropertyName] === spValue);
-
-            const thirdPropertyName = properties[2];
-
-            return mapProps[thirdPropertyName].map(thpValue => {
-
-                const itemsLv3 = itemsLv2.filter(iLv3 => iLv3[thirdPropertyName] === thpValue)[0];
-
-                return itemsLv3;
-            });
-        });
-    });
-
-    return result;
 }
 
 function getSortedMapPropertiesByNames(rowData, arrNames) {
@@ -343,7 +202,3 @@ const getCompareByKeyFn = (key = null, isStringCompare = false) => (a, b) => {
     if(valA > valB) return 1;
     return 0;
 };
-
-function getXAxesData(data) {
-
-}
